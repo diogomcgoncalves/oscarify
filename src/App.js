5176143578cbb2124db529a22b3b73b33e6b5646
Diogo Gonçalves <diogo14ads@gmail.com>
2017-08-19 19:53:34 +0100

    Created the fields and setup the structure of the app. Going to use Spotify's Implict Grant Flow for authorization method.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import spotifyConfig from './spotify_config.json';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      bandList: ''
+    };
+  }
+
+  componentDidMount(){
+    /*
+    axios.post('/user', {
+      firstName: 'Fred',
+      lastName: 'Flintstone'
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });*/
+  }
+
+  fetchArtistsTracks(){
+    console.log('====================================');
+    console.log('Fetch Tracks.');
+    console.log('====================================');
+
+    //TODO Fetch artists Ids
+    //TODO Fetch Top Tracks
+
+  }
+
   render() {
+    let {bandList} = this.state;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>OSCARIFY</h3>
+        <textarea
+          id="band-list"
+          value={bandList}
+          onChange={(e) => { this.setState({bandList: e.target.value})} } 
+        />
+        <input 
+          type="button"
+          onClick={this.fetchArtistsTracks}
+          value="Get Tracks"
+        />
       </div>
     );
   }
